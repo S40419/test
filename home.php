81a055b880695ceb7a239fd7f8892ea9c9d5cb24
S40419 <sittikorn-11@hotmail.com>
2017-07-31 00:04:23 +0700

    v1

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>KKU</title>
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+    <script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container" style="border: 2px solid orange; height: 100%; padding: 0">
+        <div style="width: 20%; height: 100%; background-color: cyan; display: inline-block; padding: 10px">
+            <a href="page1.php" target="content" class="btn btn-default" style="display: block">PAGE 1</a>
+        </div>
+        <div style="width: 80%; height: 100%; background-color: gold; float: right; padding: 4px">
+            <iframe name="content" style="border: 1px solid green; width: 100%; height: 100%; background-color: white">
+                
+        </iframe>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ page1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PAGE 1</title>
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+    <script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container" style="border: 2px solid orange; height: 100%; padding: 0">
+    <div style="width: 20%; height: 100%; background-color: cyan; display: inline-block; padding: 10px">
+
+    </div>
+    <div style="width: 80%; height: 100%; background-color: gold; float: right; padding: 4px">
+        
+    </div>
+</div>
+</body>
+</html>
