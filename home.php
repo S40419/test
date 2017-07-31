17a3e83093af7ff815c8181b09ab54b5d5d0d220
S40419 <sittikorn-11@hotmail.com>
2017-07-31 07:30:53 +0700

    2

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>KKU</title>
+    <title>COMPUTER</title>
     <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
     <script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
     <script type="text/javascript" src="./js/bootstrap.min.js"></script>
